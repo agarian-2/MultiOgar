3b3bcd1003074dfff0925102c3bee6fcaffde8bb
Tom <36286279+agarian-2@users.noreply.github.com>
2019-04-03 14:47:12 -0400

    Major Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 'use strict';
-const Log = require("./modules/Logger");
-const Commands = require("./modules/CommandList");
-const GameServer = require("./GameServer");
-const showConsole = 1;
+const Log = require("./modules/Logger"),
+    Commands = require("./modules/CommandList"),
+    GameServer = require("./GameServer"),
+    showConsole = true;
 
 function prompt() {
-    in_.question(">", function (str) {
+    in_.question(">", function(str) {
         try {
             parseCommands(str);
         } catch (error) {
@@ -19,10 +19,9 @@ function prompt() {
 function parseCommands(str) {
     Log.write(">" + str);
     if (str === '') return;
-    var split = str.split(" ");
-    var first = split[0].toLowerCase();
-    var execute = Commands.list[first];
-    if (typeof execute != 'undefined') execute(gameServer, split);
+    const split = str.split(" "),
+        execute = Commands.list[split[0].toLowerCase()];
+    if (typeof execute !== 'undefined') execute(gameServer, split);
     else Log.warn("That is an invalid Command!");
 }
 
@@ -35,8 +34,8 @@ process.on("uncaughtException", function(error) {
     Log.fatal(error.stack);
     process.exit(1);
 });
-process.argv.forEach(function (val) {
-    if (val == "--noconsole") showConsole = 0;
+process.argv.forEach(function(val) {
+    if (val == "--noconsole") showConsole = false;
     else if (val == "--help") {
         Log.print("Proper Usage: node index.js");
         Log.print("    --noconsole         Disables the console");
@@ -48,10 +47,10 @@ var gameServer = new GameServer();
 Log.info("\u001B[1m\u001B[32mMultiOgar-Edited " + gameServer.version + "\u001B[37m - An open source multi-protocol ogar server!\u001B[0m");
 gameServer.start();
 if (showConsole) {
-    var readline = require('readline');
-    var in_ = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+    var readline = require('readline'),
+        in_ = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
     setTimeout(prompt, 100);
 }
