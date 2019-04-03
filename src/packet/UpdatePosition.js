ac528c34423c0c19b96f4845f780cc58126472ba
Tom <36286279+agarian-2@users.noreply.github.com>
2019-04-03 15:14:00 -0400

    Major Update

--- src/packet/UpdatePosition.js
+++ src/packet/UpdatePosition.js
@@ -2,7 +2,7 @@
 const BinaryWriter = require("./BinaryWriter");
 
 function UpdatePos(playerTracker, x, y, scale) {
-    this.playerTracker = playerTracker,
+    this.playerTracker = playerTracker;
     this.x = x;
     this.y = y;
     this.scale = scale;
@@ -10,11 +10,11 @@ function UpdatePos(playerTracker, x, y, scale) {
 
 module.exports = UpdatePos;
 
-UpdatePos.prototype.build = function (protocol) {
+UpdatePos.prototype.build = function(protocol) {
     var writer = new BinaryWriter();
     writer.writeUInt8(0x11);
-    writer.writeFloat(this.x + this.playerTracker.scramble.X);
-    writer.writeFloat(this.y + this.playerTracker.scramble.Y);
+    writer.writeFloat(this.x + this.playerTracker.scrambleX);
+    writer.writeFloat(this.y + this.playerTracker.scrambleY);
     writer.writeFloat(this.scale);
     return writer.toBuffer();
 };
