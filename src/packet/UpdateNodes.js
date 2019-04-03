1645f54ef919baf39ed439c675132f2da78fd921
Tom <36286279+agarian-2@users.noreply.github.com>
2019-04-03 15:13:06 -0400

    Major Update

--- src/packet/UpdateNodes.js
+++ src/packet/UpdateNodes.js
@@ -1,20 +1,18 @@
 'use strict';
 function UpdateNodes(playerTracker, add, upd, eat, del) {
     this.playerTracker = playerTracker;
-    this.nodes = {
-        add: add,
-        upd: upd,
-        eat: eat,
-        del: del
-    };
+    this.addNodes = add;
+    this.updNodes = upd;
+    this.eatNodes = eat;
+    this.delNodes = del;
 }
 
 module.exports = UpdateNodes;
 
-UpdateNodes.prototype.build = function (protocol) {
+UpdateNodes.prototype.build = function(protocol) {
     if (!protocol) return null;
-    var BinaryWriter = require("./BinaryWriter");
-    var writer = new BinaryWriter();
+    var BinaryWriter = require("./BinaryWriter"),
+        writer = new BinaryWriter();
     writer.writeUInt8(0x10);
     this.writeEatItems(writer);
     if (protocol < 5) this.writeUpdateItems4(writer);
@@ -25,13 +23,13 @@ UpdateNodes.prototype.build = function (protocol) {
     return writer.toBuffer();
 };
 
-UpdateNodes.prototype.writeUpdateItems4 = function (writer) {
-    for (var i = 0; i < this.nodes.upd.length; i++) {
-        var node = this.nodes.upd[i];
+UpdateNodes.prototype.writeUpdateItems4 = function(writer) {
+    for (var i = 0; i < this.updNodes.length; i++) {
+        var node = this.updNodes[i];
         if (node.nodeID == 0) continue;
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt16(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt16(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt16(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt16(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         var color = node.color;
         writer.writeUInt8(color.r >>> 0);
@@ -40,18 +38,18 @@ UpdateNodes.prototype.writeUpdateItems4 = function (writer) {
         var flags = 0;
         if (node.spiked) flags |= 0x01;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
+        if (node.cellType === 3) flags |= 0x20;
         writer.writeUInt8(flags >>> 0);
         writer.writeUInt16(0);
     }
-    for (var i = 0; i < this.nodes.add.length; i++) {
-        node = this.nodes.add[i];
+    for (var i = 0; i < this.addNodes.length; i++) {
+        node = this.addNodes[i];
         if (node.nodeID == 0) continue;
         var cellName = null;
         if (node.owner) cellName = node.owner._nameUnicode;
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt16(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt16(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt16(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt16(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         color = node.color;
         writer.writeUInt8(color.r >>> 0);
@@ -60,7 +58,7 @@ UpdateNodes.prototype.writeUpdateItems4 = function (writer) {
         flags = 0;
         if (node.spiked) flags |= 0x01;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
+        if (node.cellType === 3) flags |= 0x20;
         writer.writeUInt8(flags >>> 0);
         if (cellName != null) writer.writeBytes(cellName);
         else writer.writeUInt16(0);
@@ -68,13 +66,13 @@ UpdateNodes.prototype.writeUpdateItems4 = function (writer) {
     writer.writeUInt32(0);
 };
 
-UpdateNodes.prototype.writeUpdateItems5 = function (writer) {
-    for (var i = 0; i < this.nodes.upd.length; i++) {
-        var node = this.nodes.upd[i];
+UpdateNodes.prototype.writeUpdateItems5 = function(writer) {
+    for (var i = 0; i < this.updNodes.length; i++) {
+        var node = this.updNodes[i];
         if (node.nodeID == 0) continue;
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt32(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt32(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt32(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt32(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         var color = node.color;
         writer.writeUInt8(color.r >>> 0);
@@ -83,22 +81,22 @@ UpdateNodes.prototype.writeUpdateItems5 = function (writer) {
         var flags = 0;
         if (node.spiked) flags |= 0x01;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
+        if (node.cellType === 3) flags |= 0x20;
         writer.writeUInt8(flags >>> 0);
         writer.writeUInt16(0);
     }
-    for (var i = 0; i < this.nodes.add.length; i++) {
-        node = this.nodes.add[i];
+    for (var i = 0; i < this.addNodes.length; i++) {
+        node = this.addNodes[i];
         if (node.nodeID == 0) continue;
-        var skinName = null;
-        var cellName = null;
+        var skinName = null,
+            cellName = null;
         if (node.owner) {
             skinName = node.owner._skinUtf8;
             cellName = node.owner._nameUnicode;
         }
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt32(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt32(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt32(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt32(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         color = node.color;
         writer.writeUInt8(color.r >>> 0);
@@ -108,7 +106,7 @@ UpdateNodes.prototype.writeUpdateItems5 = function (writer) {
         if (node.spiked) flags |= 0x01;
         if (skinName != null) flags |= 0x04;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
+        if (node.cellType === 3) flags |= 0x20;
         writer.writeUInt8(flags >>> 0);
         if (flags & 0x04) writer.writeBytes(skinName);
         if (cellName != null) writer.writeBytes(cellName);
@@ -117,20 +115,20 @@ UpdateNodes.prototype.writeUpdateItems5 = function (writer) {
     writer.writeUInt32(0 >> 0);
 };
 
-UpdateNodes.prototype.writeUpdateItems6 = function (writer) {
-    for (var i = 0; i < this.nodes.upd.length; i++) {
-        var node = this.nodes.upd[i];
+UpdateNodes.prototype.writeUpdateItems6 = function(writer) {
+    for (var i = 0; i < this.updNodes.length; i++) {
+        var node = this.updNodes[i];
         if (node.nodeID == 0) continue;
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt32(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt32(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt32(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt32(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         var flags = 0;
         if (node.spiked) flags |= 0x01;
-        if (node.cellType == 0) flags |= 0x02;
+        if (node.cellType === 0) flags |= 0x02;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
-        if (node.cellType == 1) flags |= 0x80;
+        if (node.cellType === 3) flags |= 0x20;
+        if (node.cellType === 1) flags |= 0x80;
         writer.writeUInt8(flags >>> 0);
         if (flags & 0x02) {
             var color = node.color;
@@ -139,18 +137,18 @@ UpdateNodes.prototype.writeUpdateItems6 = function (writer) {
             writer.writeUInt8(color.b >>> 0);
         }
     }
-    for (var i = 0; i < this.nodes.add.length; i++) {
-        node = this.nodes.add[i];
+    for (var i = 0; i < this.addNodes.length; i++) {
+        node = this.addNodes[i];
         if (node.nodeID == 0) continue;
-        var skinName = null;
-        var cellName = null;
+        var skinName = null,
+            cellName = null;
         if (node.owner) {
             skinName = node.owner._skinUtf8;
             cellName = node.owner._nameUtf8;
         }
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt32(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt32(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt32(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt32(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         flags = 0;
         if (node.spiked) flags |= 0x01;
@@ -158,8 +156,8 @@ UpdateNodes.prototype.writeUpdateItems6 = function (writer) {
         if (skinName != null) flags |= 0x04;
         if (cellName != null) flags |= 0x08;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
-        if (node.cellType == 1) flags |= 0x80;
+        if (node.cellType === 3) flags |= 0x20;
+        if (node.cellType === 1) flags |= 0x80;
         writer.writeUInt8(flags >>> 0);
         if (flags & 0x02) {
             color = node.color;
@@ -173,20 +171,20 @@ UpdateNodes.prototype.writeUpdateItems6 = function (writer) {
     writer.writeUInt32(0);
 };
 
-UpdateNodes.prototype.writeUpdateItems11 = function (writer) {
-    for (var i = 0; i < this.nodes.upd.length; i++) {
-        var node = this.nodes.upd[i];
+UpdateNodes.prototype.writeUpdateItems11 = function(writer) {
+    for (var i = 0; i < this.updNodes.length; i++) {
+        var node = this.updNodes[i];
         if (node.nodeID == 0) continue;
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt32(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt32(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt32(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt32(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         var flags = 0;
         if (node.spiked) flags |= 0x01;
-        if (node.cellType == 0) flags |= 0x02;
+        if (node.cellType === 0) flags |= 0x02;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
-        if (node.cellType == 1) flags |= 0x80;
+        if (node.cellType === 3) flags |= 0x20;
+        if (node.cellType === 1) flags |= 0x80;
         writer.writeUInt8(flags >>> 0);
         if (flags & 0x80) writer.writeUInt8(0x01);
         if (flags & 0x02) {
@@ -196,18 +194,18 @@ UpdateNodes.prototype.writeUpdateItems11 = function (writer) {
             writer.writeUInt8(color.b >>> 0);
         }
     }
-    for (var i = 0; i < this.nodes.add.length; i++) {
-        node = this.nodes.add[i];
+    for (var i = 0; i < this.addNodes.length; i++) {
+        node = this.addNodes[i];
         if (node.nodeID == 0) continue;
-        var skinName = null;
-        var cellName = null;
+        var skinName = null,
+            cellName = null;
         if (node.owner) {
             skinName = node.owner._skinUtf8protocol11;
             cellName = node.owner._nameUtf8;
         }
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt32(node.position.x + this.playerTracker.scramble.X >> 0);
-        writer.writeUInt32(node.position.y + this.playerTracker.scramble.Y >> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt32(node.position.x + this.playerTracker.scrambleX >> 0);
+        writer.writeUInt32(node.position.y + this.playerTracker.scrambleY >> 0);
         writer.writeUInt16(node._size >>> 0);
         flags = 0;
         if (node.spiked) flags |= 0x01;
@@ -215,8 +213,8 @@ UpdateNodes.prototype.writeUpdateItems11 = function (writer) {
         if (skinName != null) flags |= 0x04;
         if (cellName != null) flags |= 0x08;
         if (node.isAgitated) flags |= 0x10;
-        if (node.cellType == 3) flags |= 0x20;
-        if (node.cellType == 1) flags |= 0x80;
+        if (node.cellType === 3) flags |= 0x20;
+        if (node.cellType === 1) flags |= 0x80;
         writer.writeUInt8(flags >>> 0);
         if (flags & 0x80) writer.writeUInt8(0x01);
         if (flags & 0x02) {
@@ -231,27 +229,27 @@ UpdateNodes.prototype.writeUpdateItems11 = function (writer) {
     writer.writeUInt32(0);
 };
 
-UpdateNodes.prototype.writeEatItems = function (writer) {
-    writer.writeUInt16(this.nodes.eat.length >>> 0);
-    for (var i = 0; i < this.nodes.eat.length; i++) {
-        var node = this.nodes.eat[i];
-        var hunterID = 0;
+UpdateNodes.prototype.writeEatItems = function(writer) {
+    writer.writeUInt16(this.eatNodes.length >>> 0);
+    for (var i = 0; i < this.eatNodes.length; i++) {
+        var node = this.eatNodes[i],
+            hunterID = 0;
         if (node.killedBy) hunterID = node.killedBy.nodeID;
-        writer.writeUInt32((hunterID ^ this.playerTracker.scramble.ID) >>> 0);
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0); 
+        writer.writeUInt32((hunterID ^ this.playerTracker.scrambleID) >>> 0);
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0); 
     }
 };
 
-UpdateNodes.prototype.writeRemoveItems = function (writer, protocol) {
-    var length = this.nodes.eat.length + this.nodes.del.length;
+UpdateNodes.prototype.writeRemoveItems = function(writer, protocol) {
+    var length = this.eatNodes.length + this.delNodes.length;
     if (protocol < 6) writer.writeUInt32(length >>> 0);
     else writer.writeUInt16(length >>> 0);
-    for (var i = 0; i < this.nodes.eat.length; i++) {
-        var node = this.nodes.eat[i];
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
+    for (var i = 0; i < this.eatNodes.length; i++) {
+        var node = this.eatNodes[i];
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
     }
-    for (var i = 0; i < this.nodes.del.length; i++) {
-        node = this.nodes.del[i];
-        writer.writeUInt32((node.nodeID ^ this.playerTracker.scramble.ID) >>> 0);
+    for (var i = 0; i < this.delNodes.length; i++) {
+        node = this.delNodes[i];
+        writer.writeUInt32((node.nodeID ^ this.playerTracker.scrambleID) >>> 0);
     }
 };
