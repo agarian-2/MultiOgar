f298fb89ed0501e8a64e2197e26c99f7cffe1d26
Tom <36286279+agarian-2@users.noreply.github.com>
2019-04-03 15:07:26 -0400

    Major Update
    And by that, I mean removing a period...

--- src/packet/AddNode.js
+++ src/packet/AddNode.js
@@ -9,6 +9,6 @@ module.exports = AddNode;
 AddNode.prototype.build = function() {
     var buffer = new Buffer(5);
     buffer.writeUInt8(0x20, 0, 1);
-    buffer.writeUInt32LE((this.item.nodeID ^ this.playerTracker.scramble.ID) >>> 0, 1, 1);
+    buffer.writeUInt32LE((this.item.nodeID ^ this.playerTracker.scrambleID) >>> 0, 1, 1);
     return buffer;
 };
