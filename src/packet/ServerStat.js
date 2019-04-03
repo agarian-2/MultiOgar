0f93850f970dc578d164d41ef844490d061c4648
Tom <36286279+agarian-2@users.noreply.github.com>
2019-04-03 15:11:15 -0400

    Major Update

--- src/packet/ServerStat.js
+++ src/packet/ServerStat.js
@@ -1,35 +1,44 @@
 'use strict';
-function ServerStat(playerTracker) {
+function ServerStat(playerTracker, gameServer) {
     this.playerTracker = playerTracker;
+    this.gameServer = gameServer;
 }
 
 module.exports = ServerStat;
 
-ServerStat.prototype.build = function (protocol) {
-    var gameServer = this.playerTracker.gameServer;
-    var total = 0;
-    var alive = 0;
-    var spectate = 0;
+ServerStat.prototype.build = function(protocol) {
+    var gameServer = this.gameServer,
+        total = 0,
+        alive = 0,
+        spectate = 0,
+        dead = 0,
+        bots = 0;
     for (var i = 0; i < gameServer.clients.length; i++) {
         var socket = gameServer.clients[i];
+        if (socket.isConnected == null) bots++;
         if (socket == null || !socket.isConnected) continue;
         total++;
         if (socket.playerTracker.cells.length > 0) alive++;
-        else spectate++;
+        else {
+            dead++;
+            if (socket.playerTracker.isSpectating) spectate++;
+        }
     }
     var obj = {
-        'name': gameServer.config.serverName,
-        'mode': gameServer.gameMode.name,
-        'uptime': process.uptime() >>> 0,
-        'update': gameServer.updateTimeAvg.toFixed(3),
-        'playersTotal': total,
-        'playersAlive': alive,
-        'playersSpect': spectate,
-        'playersLimit': gameServer.config.serverMaxConnect
-    };
-    var json = JSON.stringify(obj);
-    var BinaryWriter = require("./BinaryWriter");
-    var writer = new BinaryWriter();
+            'name': gameServer.config.serverName,
+            'mode': gameServer.gameMode.name,
+            'uptime': process.uptime() >>> 0,
+            'update': gameServer.updateTimeAvg.toFixed(3),
+            'playersTotal': total,
+            'playersAlive': alive,
+            'playersDead': dead,
+            'playersSpect': spectate,
+            'botsTotal': bots,
+            'playersLimit': gameServer.config.serverMaxConnect
+        },
+        json = JSON.stringify(obj),
+        BinaryWriter = require("./BinaryWriter"),
+        writer = new BinaryWriter();
     writer.writeUInt8(254);
     writer.writeStringZeroUtf8(json);
     return writer.toBuffer();
