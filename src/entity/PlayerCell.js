0a28be010eb9f638bdb63748a57b11d1b5935e1c
Tom <36286279+agarian-2@users.noreply.github.com>
2022-05-28 10:29:10 -0400

    Update PlayerCell.js

--- src/entity/PlayerCell.js
+++ src/entity/PlayerCell.js
@@ -1,31 +1,28 @@
-'use strict';
-const Cell = require('./Cell'),
-    Packet = require('../packet');
+var Cell = require("./Cell");
 
 function PlayerCell() {
     Cell.apply(this, Array.prototype.slice.call(arguments));
     this.spiked = this.gameServer.config.playerSpikedCells;
     this.cellType = 0;
+    this._speed = null;
     this.canRemerge = false;
 }
 
 module.exports = PlayerCell;
 PlayerCell.prototype = new Cell;
 
-PlayerCell.prototype.canEat = function(cell) {
-    return 1;
+PlayerCell.prototype.canEat = function() {
+    return true;
 };
 
 PlayerCell.prototype.getSpeed = function(dist) {
-    var scale = 2.2 * Math.pow(this._size, -.439);
-    return (scale = 40 * scale * (this.owner.customSpeed || this.gameServer.config.playerSpeed) / 30) * (Math.min(dist, 32) / 32);
+    var scale = 2.2 * Math.pow(this._size, -.439),
+        normalized = Math.min(dist , 32) / 32;
+    return (scale = 40 * scale * (this.owner.customSpeed || this.gameServer.config.playerSpeed) / 30) * normalized;
 };
 
 PlayerCell.prototype.onAdd = function(gameServer) {
-    this.color = gameServer.config.splitRandomColor ? gameServer.randomColor() : this.owner.color;
-    this.owner.cells.push(this);
-    this.owner.socket.sendPacket(new Packet.AddNode(this.owner, this));
-    if (gameServer.config.unshift) gameServer.nodesPlayer.unshift(this);
+    if (gameServer.config.unshift === 1) gameServer.nodesPlayer.unshift(this);
     else gameServer.nodesPlayer.push(this);
     gameServer.gameMode.onCellAdd(this);
 };
