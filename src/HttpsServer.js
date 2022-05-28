3ae2614d80cdec4ca9361a21079795f1a4d1d91a
Tom <36286279+agarian-2@users.noreply.github.com>
2022-05-28 10:16:40 -0400

    Update HttpsServer.js

--- src/HttpsServer.js
+++ src/HttpsServer.js
@@ -1,26 +1,25 @@
-'use strict';
-const http = require('http'),
-    https = require('https'),
-    inherits = require('util').inherits,
+var http = require("http"),
+    https = require("https"),
+    inherits = require("util").inherits,
     httpSocketHandler = http._connectionListener,
-    Logger = require('./modules/Logger'),
+    Logger = require("./modules/Logger"),
     isOldNode = /^v0\.10\./.test(process.version);
 
 function Server(tlsconfig, requestListener) {
     if (!(this instanceof Server)) return new Server(tlsconfig, requestListener);
-    if (typeof tlsconfig === 'function') {
+    if (typeof tlsconfig === "function") {
         requestListener = tlsconfig;
         tlsconfig = undefined;
     }
-    if (typeof tlsconfig === 'object') {
-        this.removeAllListeners('connection');
+    if (typeof tlsconfig === "object") {
+        this.removeAllListeners("connection");
         https.Server.call(this, tlsconfig, requestListener);
         var event = this._events.connection;
-        if (typeof connev === 'function') this._tlsHandler = event;
+        if (typeof connev === "function") this._tlsHandler = event;
         else this._tlsHandler = event[event.length - 1];
-        this.removeListener('connection', this._tlsHandler);
+        this.removeListener("connection", this._tlsHandler);
         this._connListener = connectionListener;
-        this.on('connection', connectionListener);
+        this.on("connection", connectionListener);
         this.timeout = 120000;//2 * 60 * 1000;
         this.allowHalfOpen = false;
         this.httpAllowHalfOpen = false;
@@ -28,22 +27,22 @@ function Server(tlsconfig, requestListener) {
 }
 inherits(Server, https.Server);
 
-Server.prototype.setTimeout = function(msecs, callback) {
+Server.prototype.setTimeout = function (msecs, callback) {
     this.timeout = msecs;
-    if (callback) this.on('timeout', callback);
+    if (callback) this.on("timeout", callback);
 };
 
 Server.prototype.__httpSocketHandler = httpSocketHandler;
 
 var connectionListener;
 if (isOldNode) {
-    connectionListener = function(socket) {
+    connectionListener = function (socket) {
         var logip = socket.remoteAddress + ":" + socket.remotePort;
-        socket.on('error', function(err) {
+        socket.on("error", function (err) {
             Logger.writeError("[" + logip + "] " + err.stack);
         });
         var self = this;
-        socket.ondata = function(d, start, end) {
+        socket.ondata = function (d, start, end) {
             var firstByte = d[start];
             if (firstByte < 32 || firstByte >= 127) {
                 socket.ondata = null;
@@ -56,17 +55,18 @@ if (isOldNode) {
         };
     };
 } else {
-    connectionListener = function(socket) {
+    connectionListener = function (socket) {
         var logip = socket.remoteAddress + ":" + socket.remotePort;
-        socket.on('error', function(err) {
+        socket.on("error", function (err) {
             Logger.writeError("[" + logip + "] " + err.stack);
         });
         var self = this,
             data = socket.read(1);
-        if (data === null) socket.once('readable', function() {
-            self._connListener(socket);
-        });
-        else {
+        if (data === null) {
+            socket.once("readable", function () {
+                self._connListener(socket);
+            });
+        } else {
             var firstByte = data[0];
             socket.unshift(data);
             if (firstByte < 32 || firstByte >= 127) this._tlsHandler(socket);
@@ -77,6 +77,6 @@ if (isOldNode) {
 
 exports.Server = Server;
 
-exports.createServer = function(tlsconfig, requestListener) {
+exports.createServer = function (tlsconfig, requestListener) {
     return new Server(tlsconfig, requestListener);
 };
