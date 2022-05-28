6812162b72d97c7d0df50c66eefa661d6fc4c65d
Tom <36286279+agarian-2@users.noreply.github.com>
2022-05-28 10:21:38 -0400

    Update BotLoader.js

--- src/ai/BotLoader.js
+++ src/ai/BotLoader.js
@@ -1,36 +1,42 @@
-'use strict';
-const FakeSocket = require("./FakeSocket"),
+var FakeSocket = require("./FakeSocket"),
     PacketHandler = require("../PacketHandler");
 
 function BotLoader(gameServer) {
     this.gameServer = gameServer;
     this.loadNames();
+    this.loadSkins();
 }
 
 module.exports = BotLoader;
 
 BotLoader.prototype.getName = function() {
-    var name = "";
-    if (this.randomNames.length > 0) {
-        var index = (this.randomNames.length * Math.random()) >>> 0;
-        name = this.randomNames[index];
-    } else name = "bot" + ++this.nameIndex;
-    return name;
+    var skin = "",
+        name = "";
+    if (this.randomSkins.length > 0 && Math.random() < .75) skin = "{" + this.randomSkins[(this.randomSkins.length * Math.random()) >>> 0] + "}";
+    if (this.randomNames.length > 0) name = this.randomNames[(this.randomNames.length * Math.random()) >>> 0];
+    else name = "bot" + ++this.nameIndex;
+    return skin + name;
 };
 
 BotLoader.prototype.loadNames = function() {
     this.randomNames = [];
     var fs = require("fs");
-    if (fs.existsSync("../src/ai/BotNames.txt")) {
-        this.randomNames = fs.readFileSync("../src/ai/BotNames.txt", "utf8").split(/[\r\n]+/).filter(function(x) {
-            return x != '';
-        });
-    }
+    if (fs.existsSync("../src/ai/BotNames.txt")) this.randomNames = fs.readFileSync("../src/ai/BotNames.txt", "utf8").split(/[\r\n]+/).filter(function (x) {
+        return x !== "";
+    });
     this.nameIndex = 0;
 };
 
+BotLoader.prototype.loadSkins = function() {
+    this.randomSkins = [];
+    var fs = require("fs");
+    if (fs.existsSync("../src/ai/Skins.txt")) this.randomSkins = fs.readFileSync("../src/ai/Skins.txt", "utf8").split(/[\r\n]+/).filter(function (x) {
+        return x !== "";
+    });
+};
+
 BotLoader.prototype.addBot = function() {
-    var BotPlayer = require('./BotPlayer'),
+    var BotPlayer = require("./BotPlayer"),
         socket = new FakeSocket(this.gameServer);
     socket.playerTracker = new BotPlayer(this.gameServer, socket);
     socket.packetHandler = new PacketHandler(this.gameServer, socket);
@@ -39,15 +45,13 @@ BotLoader.prototype.addBot = function() {
 };
 
 BotLoader.prototype.addMinion = function(owner, name) {
-    var MinionPlayer = require('./MinionPlayer'),
+    var MinionPlayer = require("./MinionPlayer"),
         socket = new FakeSocket(this.gameServer);
     socket.playerTracker = new MinionPlayer(this.gameServer, socket, owner);
     socket.packetHandler = new PacketHandler(this.gameServer, socket);
     socket.playerTracker.owner = owner;
     this.gameServer.clients.push(socket);
-    if (typeof name == "undefined" || name === "") {
-        this.gameServer.config.minionSameName ? name = socket.playerTracker.owner._name :
-        name = this.gameServer.config.minionDefaultName;
-    }
+    owner.minions.push(socket.playerTracker);
+    if (typeof name === "undefined" || name === "") this.gameServer.config.minionSameName ? name = socket.playerTracker.owner._name : name = this.gameServer.config.minionDefaultName;
     socket.packetHandler.nickName(name);
 };
