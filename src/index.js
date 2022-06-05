24991a371b67d21b419e9e962e3ca2cdcc4d9450
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-05 12:20:50 -0400

    Temporarily revert ES6 conversion

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-"use strict";
-const Log = require("./modules/Logger");
-const Commands = require("./modules/CommandList");
-const GameServer = require("./GameServer");
-const readline = require("readline");
-const in_ = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-const gameServer = new GameServer();
-const parseCommands = str => {
-    Log.write(">" + str);
-    if (str === "") return;
-    let split = str.split(" "),
-        first = split[0].toLowerCase(),
-        execute = Commands.list[first];
-    if (typeof execute !== "undefined") execute(gameServer, split);
-    else Log.warn("That is an invalid command.");
-};
-const prompt = () => {
-    in_.question(">", str => {
+var Log = require("./modules/Logger"),
+    Commands = require("./modules/CommandList"),
+    GameServer = require("./GameServer"),
+    showConsole = true;
+
+function prompt() {
+    in_.question(">", function (str) {
         try {
             parseCommands(str);
         } catch (error) {
@@ -27,28 +13,44 @@ const prompt = () => {
             setTimeout(prompt, 0);
         }
     });
-};
-let showConsole = true;
+}
+
+function parseCommands(str) {
+    Log.write(">" + str);
+    if (str === "") return;
+    var split = str.split(" "),
+        first = split[0].toLowerCase(),
+        execute = Commands.list[first];
+    if (typeof execute !== "undefined") execute(gameServer, split);
+    else Log.warn("That is an invalid command.");
+}
 
 Log.start();
-process.on("exit", error => {
+process.on("exit", function(error) {
     Log.debug("process.exit(" + error + ")");
     Log.shutdown();
 });
-process.on("uncaughtException", error => {
+process.on("uncaughtException", function(error) {
     Log.fatal(error.stack);
     process.exit(1);
 });
-process.argv.forEach(value => {
-    if (value === "--noconsole") showConsole = false;
-    else if (value === "--help") {
+process.argv.forEach(function (val) {
+    if (val === "--noconsole") showConsole = false;
+    else if (val === "--help") {
         Log.print("Proper Usage: node index.js");
         Log.print("    --noconsole         Disables the console");
         Log.print("    --help              Help menu.");
         Log.print("");
     }
 });
-
+var gameServer = new GameServer();
 Log.info("\u001B[1m\u001B[32mMultiOgar-Edited " + gameServer.version + "\u001B[37m - An open source multi-protocol ogar server!\u001B[0m");
 gameServer.start();
-if (showConsole) setTimeout(prompt, 100);
+if (showConsole === true) {
+    var readline = require("readline"),
+        in_ = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    setTimeout(prompt, 100);
+}
