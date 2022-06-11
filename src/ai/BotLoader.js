8efb493cc01b60f29e44f751c3c22ed1ebce3be7
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-11 18:43:05 -0400

    Update BotLoader.js to ES6

--- src/ai/BotLoader.js
+++ src/ai/BotLoader.js
@@ -1,57 +1,51 @@
-var FakeSocket = require("./FakeSocket"),
-    PacketHandler = require("../PacketHandler");
+"use strict";
+const BotPlayer = require("./BotPlayer");
+const MinionPlayer = require("./MinionPlayer");
+const FakeSocket = require("./FakeSocket");
+const PacketHandler = require("../PacketHandler");
 
-function BotLoader(gameServer) {
-    this.gameServer = gameServer;
-    this.loadNames();
-    this.loadSkins();
+class BotLoader {
+    constructor(gameServer) {
+        this.gameServer = gameServer;
+        this.loadNames();
+        this.loadSkins();
+    }
+    getName() {
+        let skin = "",
+            name = "";
+        if (this.randomSkins.length > 0 && Math.random() < .75) skin = "{" + this.randomSkins[(this.randomSkins.length * Math.random()) >>> 0] + "}";
+        if (this.randomNames.length > 0) name = this.randomNames[(this.randomNames.length * Math.random()) >>> 0];
+        else name = "bot" + ++this.nameIndex;
+        return skin + name;
+    }
+    loadNames() {
+        this.randomNames = [];
+        let fs = require("fs");
+        if (fs.existsSync("../src/ai/BotNames.txt")) this.randomNames = fs.readFileSync("../src/ai/BotNames.txt", "utf8").split(/[\r\n]+/).filter(x => x !== "");
+        this.nameIndex = 0;
+    }
+    loadSkins() {
+        this.randomSkins = [];
+        let fs = require("fs");
+        if (fs.existsSync("../src/ai/Skins.txt")) this.randomSkins = fs.readFileSync("../src/ai/Skins.txt", "utf8").split(/[\r\n]+/).filter(x => x !== "");
+    }
+    addBot() {
+        let socket = new FakeSocket(this.gameServer);
+        socket.playerTracker = new BotPlayer(this.gameServer, socket);
+        socket.packetHandler = new PacketHandler(this.gameServer, socket);
+        this.gameServer.clients.push(socket);
+        socket.packetHandler.nickName(this.getName());
+    }
+    addMinion(owner, name) {
+        let socket = new FakeSocket(this.gameServer);
+        socket.playerTracker = new MinionPlayer(this.gameServer, socket, owner);
+        socket.packetHandler = new PacketHandler(this.gameServer, socket);
+        socket.playerTracker.owner = owner;
+        this.gameServer.clients.push(socket);
+        owner.minions.push(socket.playerTracker);
+        if (typeof name === "undefined" || name === "") name = this.gameServer.config.minionSameName ? socket.playerTracker.owner._name : this.gameServer.config.minionDefaultName;
+        socket.packetHandler.nickName(name);
+    }
 }
 
 module.exports = BotLoader;
-
-BotLoader.prototype.getName = function() {
-    var skin = "",
-        name = "";
-    if (this.randomSkins.length > 0 && Math.random() < .75) skin = "{" + this.randomSkins[(this.randomSkins.length * Math.random()) >>> 0] + "}";
-    if (this.randomNames.length > 0) name = this.randomNames[(this.randomNames.length * Math.random()) >>> 0];
-    else name = "bot" + ++this.nameIndex;
-    return skin + name;
-};
-
-BotLoader.prototype.loadNames = function() {
-    this.randomNames = [];
-    var fs = require("fs");
-    if (fs.existsSync("../src/ai/BotNames.txt")) this.randomNames = fs.readFileSync("../src/ai/BotNames.txt", "utf8").split(/[\r\n]+/).filter(function (x) {
-        return x !== "";
-    });
-    this.nameIndex = 0;
-};
-
-BotLoader.prototype.loadSkins = function() {
-    this.randomSkins = [];
-    var fs = require("fs");
-    if (fs.existsSync("../src/ai/Skins.txt")) this.randomSkins = fs.readFileSync("../src/ai/Skins.txt", "utf8").split(/[\r\n]+/).filter(function (x) {
-        return x !== "";
-    });
-};
-
-BotLoader.prototype.addBot = function() {
-    var BotPlayer = require("./BotPlayer"),
-        socket = new FakeSocket(this.gameServer);
-    socket.playerTracker = new BotPlayer(this.gameServer, socket);
-    socket.packetHandler = new PacketHandler(this.gameServer, socket);
-    this.gameServer.clients.push(socket);
-    socket.packetHandler.nickName(this.getName());
-};
-
-BotLoader.prototype.addMinion = function(owner, name) {
-    var MinionPlayer = require("./MinionPlayer"),
-        socket = new FakeSocket(this.gameServer);
-    socket.playerTracker = new MinionPlayer(this.gameServer, socket, owner);
-    socket.packetHandler = new PacketHandler(this.gameServer, socket);
-    socket.playerTracker.owner = owner;
-    this.gameServer.clients.push(socket);
-    owner.minions.push(socket.playerTracker);
-    if (typeof name === "undefined" || name === "") this.gameServer.config.minionSameName ? name = socket.playerTracker.owner._name : name = this.gameServer.config.minionDefaultName;
-    socket.packetHandler.nickName(name);
-};
