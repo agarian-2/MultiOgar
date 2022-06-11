99563401f41c5c830f3ebba2dd380ac55cb4d0b2
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-11 18:55:48 -0400

    Update PlayerCell.js to ES6

--- src/entity/PlayerCell.js
+++ src/entity/PlayerCell.js
@@ -1,36 +1,34 @@
-var Cell = require("./Cell");
+"use strict";
+const Cell = require("./Cell");
 
-function PlayerCell() {
-    Cell.apply(this, Array.prototype.slice.call(arguments));
-    this.spiked = this.gameServer.config.playerSpikedCells;
-    this.cellType = 0;
-    this._speed = null;
-    this.canRemerge = false;
+class PlayerCell extends Cell {
+    constructor(gameServer, owner, position, size) {
+        super(gameServer, owner, position, size);
+        this.spiked = this.gameServer.config.playerSpikedCells;
+        this.cellType = 0;
+        this._speed = null;
+        this.canRemerge = false;
+    }
+    canEat() {
+        return true;
+    }
+    getSpeed(dist) {
+        let speed = 2.2 * Math.pow(this._size, -.45) * 40;
+        speed *= (this.owner.customSpeed || this.gameServer.config.playerSpeed) / 30;
+        return speed * Math.min(dist , 32) / 32;
+    }
+    onAdd(gameServer) {
+        if (gameServer.config.unshift === 1) gameServer.nodesPlayer.unshift(this);
+        else gameServer.nodesPlayer.push(this);
+        gameServer.gameMode.onCellAdd(this);
+    }
+    onRemove(gameServer) {
+        let index = this.owner.cells.indexOf(this);
+        if (index !== -1) this.owner.cells.splice(index, 1);
+        index = gameServer.nodesPlayer.indexOf(this);
+        if (index !== -1) gameServer.nodesPlayer.splice(index, 1);
+        gameServer.gameMode.onCellRemove(this);
+    }
 }
 
 module.exports = PlayerCell;
-PlayerCell.prototype = new Cell;
-
-PlayerCell.prototype.canEat = function() {
-    return true;
-};
-
-PlayerCell.prototype.getSpeed = function(dist) {
-    var scale = 2.2 * Math.pow(this._size, -.439),
-        normalized = Math.min(dist , 32) / 32;
-    return (scale = 40 * scale * (this.owner.customSpeed || this.gameServer.config.playerSpeed) / 30) * normalized;
-};
-
-PlayerCell.prototype.onAdd = function(gameServer) {
-    if (gameServer.config.unshift === 1) gameServer.nodesPlayer.unshift(this);
-    else gameServer.nodesPlayer.push(this);
-    gameServer.gameMode.onCellAdd(this);
-};
-
-PlayerCell.prototype.onRemove = function(gameServer) {
-    var index = this.owner.cells.indexOf(this);
-    if (index !== -1) this.owner.cells.splice(index, 1);
-    index = gameServer.nodesPlayer.indexOf(this);
-    if (index !== -1) gameServer.nodesPlayer.splice(index, 1);
-    gameServer.gameMode.onCellRemove(this);
-};
