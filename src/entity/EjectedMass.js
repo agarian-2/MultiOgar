07c8e29598f94f8f7d08e5d52a2e152ecb056613
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-11 18:46:12 -0400

    Update EjectedMass to ES6

--- src/entity/EjectedMass.js
+++ src/entity/EjectedMass.js
@@ -1,18 +1,18 @@
-var Cell = require("./Cell");
+"use strict";
+const Cell = require("./Cell");
 
-function EjectedMass() {
-    Cell.apply(this, Array.prototype.slice.call(arguments));
-    this.cellType = 3;
+class EjectedMass extends Cell {
+    constructor(gameServer, owner, position, size) {
+        super(gameServer, owner, position, size);
+        this.cellType = 3;
+    }
+    onAdd(gameServer) {
+        gameServer.nodesEject.push(this);
+    }
+    onRemove(gameServer) {
+        let index = gameServer.nodesEject.indexOf(this);
+        if (index !== -1) gameServer.nodesEject.splice(index, 1);
+    }
 }
 
 module.exports = EjectedMass;
-EjectedMass.prototype = new Cell;
-
-EjectedMass.prototype.onAdd = function(gameServer) {
-    gameServer.nodesEject.push(this);
-};
-
-EjectedMass.prototype.onRemove = function(gameServer) {
-    var index = gameServer.nodesEject.indexOf(this);
-    if (index !== -1) gameServer.nodesEject.splice(index, 1);
-};
