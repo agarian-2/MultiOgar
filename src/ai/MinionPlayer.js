5005554814f20a66cea47469995ef31710a91e19
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-11 18:44:45 -0400

    Update MinionPlayer.js to ES6

--- src/ai/MinionPlayer.js
+++ src/ai/MinionPlayer.js
@@ -1,50 +1,50 @@
-var PlayerTracker = require("../PlayerTracker");
+"use strict";
+const PlayerTracker = require("../PlayerTracker");
 
-function MinionPlayer() {
-    PlayerTracker.apply(this, Array.prototype.slice.call(arguments));
-    this.isMi = true;
-}
-
-module.exports = MinionPlayer;
-
-MinionPlayer.prototype = new PlayerTracker;
-
-MinionPlayer.prototype.checkConnection = function() { // TO DO (maybe): Minion color change with X
-    if (this.gameServer.config.minionSameColor) {
-        this.color = this.owner.color;
-        this.cells.forEach(function(item) {
-            item.color = this.owner.color;
-        }, this);
+class MinionPlayer extends PlayerTracker {
+    constructor(gameServer, socket) {
+        super(gameServer, socket);
+        this.isMi = true;
     }
-    if (this.socket.isCloseReq) {
-        for (;this.cells.length;) this.gameServer.removeNode(this.cells[0]);
-        return this.isRemoved = true;
-    }
-    if (!this.cells.length) {
-        this.gameServer.gameMode.onPlayerSpawn(this.gameServer, this);
-        if (!this.cells.length) this.socket.close();
-    }
-    if (this.owner.socket.isConnected === false || !this.owner.minion.control) this.socket.close();
-    this.frozen = this.owner.minion.frozen ? true : false;
-    if (this.owner.minion.split) this.socket.packetHandler.pressSpace = true;
-    if (this.owner.minion.eject) this.socket.packetHandler.pressW = true;
-    if (this.owner.minion.follow) this.mouse = this.owner.centerPos;
-    else this.mouse = this.owner.mouse;
-    if (this.owner.minion.collect) {
-        this.viewNodes = [];
-        var self = this;
-        this.gameServer.quadTree.find(this.viewBox, function(item) {
-            if (item.cell.cellType === 1 || item.cell.cellType === 3) self.viewNodes.push(item.cell);
-        });
-        var bestDist = 1 / 0; // Infinite
-        for (var i in this.viewNodes) {
-            var node = this.viewNodes[i],
-                dx = this.cells[0].position.x - node.position.x,
-                dy = this.cells[0].position.y - node.position.y;
-            if (dx * dx + dy * dy < bestDist) {
-                bestDist = dx * dx + dy * dy;
-                this.mouse = node.position;
+    checkConnection() {
+        if (this.gameServer.config.minionSameColor) {
+            this.color = this.owner.color;
+            this.cells.forEach(item => {
+                item.color = this.owner.color;
+            }, this);
+        }
+        if (this.socket.isCloseReq) {
+            for (;this.cells.length;) this.gameServer.removeNode(this.cells[0]);
+            return this.isRemoved = true;
+        }
+        if (!this.cells.length) {
+            this.gameServer.gameMode.onPlayerSpawn(this.gameServer, this);
+            if (!this.cells.length) this.socket.close();
+        }
+        if (this.owner.socket.isConnected === false || !this.owner.minion.control) this.socket.close();
+        this.frozen = this.owner.minion.frozen ? true : false;
+        if (this.owner.minion.split) this.socket.packetHandler.pressSpace = true;
+        if (this.owner.minion.eject) this.socket.packetHandler.pressW = true;
+        if (this.owner.minion.follow) this.mouse = this.owner.centerPos;
+        else this.mouse = this.owner.mouse;
+        if (this.owner.minion.collect) {
+            this.viewNodes = [];
+            let self = this;
+            this.gameServer.quadTree.find(this.viewBox, item => {
+                if (item.cell.cellType === 1 || item.cell.cellType === 3) self.viewNodes.push(item.cell);
+            });
+            let bestDist = Infinity;
+            for (let i in this.viewNodes) {
+                let node = this.viewNodes[i],
+                    dx = this.cells[0].position.x - node.position.x,
+                    dy = this.cells[0].position.y - node.position.y;
+                if (dx * dx + dy * dy < bestDist) {
+                    bestDist = dx * dx + dy * dy;
+                    this.mouse = node.position;
+                }
             }
         }
     }
-};
+}
+
+module.exports = MinionPlayer;
