b771ad011e50bfba6e1b1c1bcd5e49c5a8837b0c
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-11 18:56:10 -0400

    Update Virus.js to ES6

--- src/entity/Virus.js
+++ src/entity/Virus.js
@@ -1,69 +1,72 @@
-var Cell = require("./Cell");
+"use strict";
+const Cell = require("./Cell");
 
-function Virus() {
-    Cell.apply(this, Array.prototype.slice.call(arguments));
-    this.cellType = 2;
-    this.spiked = true;
-    this.isMotherCell = false;
-    if (!this.gameServer.config.virusRandomColor) this.color = {
-        r: 51,
-        g: 255,
-        b: 51
-    };
-    else this.color = this.gameServer.randomColor();
-}
-
-module.exports = Virus;
-Virus.prototype = new Cell;
-
-Virus.prototype.canEat = function(prey) {
-    if (this.gameServer.nodesVirus.length < this.gameServer.config.virusMaxAmount) return prey.cellType === 3;
-};
-
-Virus.prototype.onEat = function(prey) {
-    if (this.gameServer.config.virusPush) this.setBoost(this.gameServer.config.virusEjectSpeed - 460, Math.atan2(prey.boostDirection.x, prey.boostDirection.y));
-    else {
-        this.setSize(Math.sqrt(this.radius + prey.radius));
-        if (this._size >= this.gameServer.config.virusMaxSize) {
-            this.setSize(this.gameServer.config.virusMinSize);
-            this.gameServer.shootVirus(this, prey.boostDirection.angle);
+class Virus extends Cell {
+    constructor(gameServer, owner, position, size) {
+        super(gameServer, owner, position, size);
+        this.cellType = 2;
+        this.spiked = true;
+        this.isMotherCell = false;
+        this.color = this.gameServer.config.virusRandomColor ? this.gameServer.randomColor() : {
+            r: 51,
+            g: 255,
+            b: 51
+        };
+    }
+    canEat(cell) {
+        if (this.gameServer.nodesVirus.length < this.gameServer.config.virusMaxAmount) return cell.cellType === 3;
+    }
+    onEat(cell) {
+        if (this.gameServer.config.virusPush) this.setBoost(this.gameServer.config.virusEjectSpeed - 460, Math.atan2(cell.boostDirection.x, cell.boostDirection.y));
+        else {
+            this.setSize(Math.sqrt(this.radius + cell.radius));
+            if (this._size >= this.gameServer.config.virusMaxSize) {
+                this.setSize(this.gameServer.config.virusMinSize);
+                this.gameServer.shootVirus(this, cell.boostDirection.angle);
+            }
         }
     }
-};
-
-Virus.prototype.onEaten = function(cell) {
-    if (!cell.owner) return;
-    const config = this.gameServer.config;
-    var cellsLeft = (config.virusMaxCells || config.playerMaxCells) - cell.owner.cells.length;
-    if (cellsLeft) {
-        var splitCount,
+    onEaten(cell) {
+        if (!cell.owner) return;
+        let config = this.gameServer.config,
+            cellsLeft = (config.virusMaxCells || config.playerMaxCells) - cell.owner.cells.length;
+        if (cellsLeft <= 0) return;
+        let splitCount,
+            splitMass,
             splitMin = config.virusSplitDiv,
-            mass = cell._mass,
+            cellMass = cell._mass,
             splits = [];
-        if (mass / cellsLeft < splitMin) {
-            for (splitMass = mass / (splitCount = 2); splitMass > splitMin && 2 * splitCount < cellsLeft;) splitMass = mass / (splitCount *= 2);
-            for (splitMass = mass / (splitCount + 1); splitCount-- > 0;) splits.push(splitMass);
+        if (cellMass / cellsLeft < splitMin) {
+            splitCount = 2;
+            splitMass = cellMass / splitCount;
+            while (splitMass > splitMin && 2 * splitCount < cellsLeft) splitMass = cellMass / (splitCount *= 2);
+            splitMass = cellMass / (splitCount + 1);
+            while (splitCount-- > 0) splits.push(splitMass);
             return this.explode(cell, splits);
         }
-        for (var splitMass = mass / 2, massLeft = mass / 2; cellsLeft-- > 0;) {
-            if (massLeft / cellsLeft < splitMin) for (splitMass = massLeft / cellsLeft; cellsLeft-- > 0;) splits.push(splitMass);
-            for (;splitMass >= massLeft && cellsLeft > 0;) splitMass /= 2;
+        let massLeft = cellMass / 2;
+        splitMass = cellMass / 2;
+        while (cellsLeft-- > 0) {
+            if (massLeft / cellsLeft < splitMin) {
+                splitMass = massLeft / cellsLeft;
+                while (cellsLeft-- > 0) splits.push(splitMass);
+            }
+            while (splitMass >= massLeft && cellsLeft > 0) splitMass /= 2;
             splits.push(splitMass);
             massLeft -= splitMass;
         }
         this.explode(cell, splits);
     }
-};
+    explode(cell, splits) {
+        for (let i = 0; i < splits.length; i++) this.gameServer.splitPlayerCell(cell.owner, cell, Math.PI * 2 * Math.random(), splits[i]);
+    }
+    onAdd(gameServer) {
+        gameServer.nodesVirus.push(this);
+    }
+    onRemove(gameServer) {
+        let index = gameServer.nodesVirus.indexOf(this);
+        if (index !== -1) gameServer.nodesVirus.splice(index, 1);
+    }
+}
 
-Virus.prototype.explode = function(cell, splits) {
-    for (var i = 0; i < splits.length; i++) this.gameServer.splitPlayerCell(cell.owner, cell, 2 * Math.PI * Math.random(), splits[i]);
-};
-
-Virus.prototype.onAdd = function(gameServer) {
-    gameServer.nodesVirus.push(this);
-};
-
-Virus.prototype.onRemove = function(gameServer) {
-    var index = gameServer.nodesVirus.indexOf(this);
-    if (index !== -1) gameServer.nodesVirus.splice(index, 1);
-};
+module.exports = Virus;
