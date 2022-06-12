dbdc08a837fed7493172da33b17da8b771ded23a
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-12 16:49:25 -0400

    Update index.js to ES6
    This also changes the command parsing to work with the new system.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-var Log = require("./modules/Logger"),
-    Commands = require("./modules/CommandList"),
-    GameServer = require("./GameServer"),
-    showConsole = true;
-
-function prompt() {
-    in_.question(">", function (str) {
+"use strict";
+const Log = require("./modules/Logger");
+const Commands = require("./modules/CommandList");
+const GameServer = require("./GameServer");
+const readline = require("readline");
+const parseCommands = str => {
+    Log.write(">" + str);
+    if (str === "") return;
+    let split = str.split(" ");
+    if (Commands[split[0].toLowerCase()]) Commands[split[0].toLowerCase()](gameServer, split);
+    else Log.warn("That is an invalid command.");
+};
+let in_;
+const prompt = () => {
+    in_.question(">", str => {
         try {
             parseCommands(str);
-        } catch (error) {
+        } catch(error) {
             Log.error(error.stack);
         } finally {
             setTimeout(prompt, 0);
         }
     });
-}
-
-function parseCommands(str) {
-    Log.write(">" + str);
-    if (str === "") return;
-    var split = str.split(" "),
-        first = split[0].toLowerCase(),
-        execute = Commands.list[first];
-    if (typeof execute !== "undefined") execute(gameServer, split);
-    else Log.warn("That is an invalid command.");
-}
+};
+let showConsole = true;
 
 Log.start();
-process.on("exit", function(error) {
+
+process.on("exit", error => {
     Log.debug("process.exit(" + error + ")");
     Log.shutdown();
 });
-process.on("uncaughtException", function(error) {
+process.on("uncaughtException", error => {
     Log.fatal(error.stack);
     process.exit(1);
 });
-process.argv.forEach(function (val) {
+process.argv.forEach(val => {
     if (val === "--noconsole") showConsole = false;
     else if (val === "--help") {
         Log.print("Proper Usage: node index.js");
@@ -43,14 +43,14 @@ process.argv.forEach(function (val) {
         Log.print("");
     }
 });
-var gameServer = new GameServer();
+
+let gameServer = new GameServer();
 Log.info("\u001B[1m\u001B[32mMultiOgar-Edited " + gameServer.version + "\u001B[37m - An open source multi-protocol ogar server!\u001B[0m");
 gameServer.start();
-if (showConsole === true) {
-    var readline = require("readline"),
-        in_ = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+if (showConsole) {
+    in_ = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
     setTimeout(prompt, 100);
 }
