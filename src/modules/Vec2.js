743ef96b2d2b1edbf7228c6ba3dce81a85b055c9
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-12 18:36:40 -0400

    Update Vec2.js to ES6

--- src/modules/Vec2.js
+++ src/modules/Vec2.js
@@ -1,70 +1,63 @@
-function Vec2(x, y) {
-    this.x = x,
-    this.y = y;
+"use strict";
+
+class Vec2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    add(x, y) {
+        if (x instanceof Vec2) {
+            this.x += x.x;
+            this.y += x.y;
+        } else {
+            this.x += x;
+            this.y += y;
+        }
+        return this;
+    }
+    add2(d, m) {
+        this.x += d.x * m;
+        this.y += d.y * m;
+        return this;
+    }
+    sub(x, y) {
+        if (x instanceof Vec2) {
+            this.x -= x.x;
+            this.y -= x.y;
+        } else {
+            this.x -= x;
+            this.y -= y;
+        }
+        return this;
+    }
+    sub2(d, m) {
+        this.x -= d.x * m;
+        this.y -= d.y * m;
+        return this;
+    }
+    angle() {
+        return Math.atan2(this.x, this.y);
+    }
+    clone() {
+        return new Vec2(this.x, this.y);
+    }
+    dist(d) {
+        return ~~d.x * ~~d.x + ~~d.y * ~~d.y;
+    }
+    sqDist(d) {
+        return Math.sqrt(d.x * d.x + d.y * d.y);
+    }
+    length() {
+        return this.sqDist(this);
+    }
+    normalize() {
+        return this.scale(1 / this.length());
+    }
+    scale(x, y) {
+        this.x *= x;
+        this.y *= y || x;
+        return this;
+    }
 }
-
-Vec2.prototype.add = function(x, y) {
-    if (x instanceof Vec2) {
-        this.x += x.x;
-        this.y += x.y;
-    } else {
-        this.x += x;
-        this.y += y;
-    }
-    return this;
-};
-
-Vec2.prototype.add2 = function(d, m) {
-    this.x += d.x * m;
-    this.y += d.y * m;
-    return this;
-};
-
-Vec2.prototype.sub = function(x, y) {
-    if (x instanceof Vec2) {
-        this.x -= x.x;
-        this.y -= x.y;
-    } else {
-        this.x -= x;
-        this.y -= y;
-    }
-    return this;
-};
-
-Vec2.prototype.sub2 = function(d, m) {
-    this.x -= d.x * m;
-    this.y -= d.y * m;
-    return this;
-};
-
-Vec2.prototype.angle = function() {
-    return Math.atan2(this.x, this.y);
-};
-
-Vec2.prototype.clone = function() {
-    return new Vec2(this.x, this.y);
-};
-
-Vec2.prototype.dist = function(d) {
-    return ~~d.x * ~~d.x + ~~d.y * ~~d.y;
-};
-
-Vec2.prototype.sqDist = function(d) {
-    return Math.sqrt(d.x * d.x + d.y * d.y);
-};
-
-Vec2.prototype.length = function() {
-    return this.sqDist(this);
-};
-
-Vec2.prototype.normalize = function() {
-    return this.scale(1 / this.length());
-};
-
-Vec2.prototype.scale = function(x, y) {
-    this.x *= x;
-    this.y *= y || x;
-    return this;
-};
 
 module.exports = Vec2;
