bf482579a181d87b0df2a7e164d0af8e18664ef2
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-11 20:15:02 -0400

    Update Mode.js to ES6

--- src/gamemodes/Mode.js
+++ src/gamemodes/Mode.js
@@ -1,42 +1,36 @@
-function Mode() {
-    this.ID = -1;
-    this.name = "Null";
-    this.decayMod = 1;
-    this.packetLB = 49;
-    this.isTeams = false;
-    this.isTournament = false;
+"use strict";
+
+class Mode {
+    constructor() {
+        this.ID = -1;
+        this.name = "Null";
+        this.decayMod = 1;
+        this.packetLB = 49;
+        this.isTeams = false;
+        this.isTournament = false;
+    }
+    onServerInit(gameServer) {
+        gameServer.running = true;
+    }
+    onTick() {}
+    onPlayerInit() {}
+    onPlayerSpawn(gameServer, player) {
+        player.color = gameServer.randomColor();
+        gameServer.spawnPlayer(player);
+    }
+    onPlayerDeath() {}
+    onCellAdd() {}
+    onCellRemove() {}
+    onCellMove() {}
+    updateLB(gameServer) { // Rename to updateLeaderboard later
+        gameServer.leaderboardType = this.packetLB;
+    }
+    onChange(gameServer) {
+        for (;gameServer.nodesAll.length;) gameServer.removeNode(gameServer.nodesAll[0]);
+        for (;gameServer.nodesEject.length;) gameServer.removeNode(gameServer.nodesEject[0]);
+        for (;gameServer.nodesFood.length;) gameServer.removeNode(gameServer.nodesFood[0]);
+        for (;gameServer.nodesVirus.length;) gameServer.removeNode(gameServer.nodesVirus[0]);
+    }
 }
 
 module.exports = Mode;
-
-Mode.prototype.onServerInit = function(gameServer) {
-    gameServer.running = true;
-};
-
-Mode.prototype.onTick = function() {};
-
-Mode.prototype.onPlayerInit = function() {};
-
-Mode.prototype.onPlayerSpawn = function(gameServer, player) {
-    player.color = gameServer.randomColor();
-    gameServer.spawnPlayer(player);
-};
-
-Mode.prototype.onPlayerDeath = function() {};
-
-Mode.prototype.onCellAdd = function() {};
-
-Mode.prototype.onCellRemove = function() {};
-
-Mode.prototype.onCellMove = function() {};
-
-Mode.prototype.updateLB = function(gameServer) {
-    gameServer.leaderboardType = this.packetLB;
-};
-
-Mode.prototype.onChange = function(gameServer) {
-    for (;gameServer.nodesAll.length;) gameServer.removeNode(gameServer.nodesAll[0]);
-    for (;gameServer.nodesEject.length;) gameServer.removeNode(gameServer.nodesEject[0]);
-    for (;gameServer.nodesFood.length;) gameServer.removeNode(gameServer.nodesFood[0]);
-    for (;gameServer.nodesVirus.length;) gameServer.removeNode(gameServer.nodesVirus[0]);
-};
