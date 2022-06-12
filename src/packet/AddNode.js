f797e2262789060205a38f4964fb8b7afff5f8ba
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-12 18:52:03 -0400

    Update AddNode.js to ES6

--- src/packet/AddNode.js
+++ src/packet/AddNode.js
@@ -1,13 +1,16 @@
-function AddNode(playerTracker, item) {
-    this.playerTracker = playerTracker;
-    this.item = item;
+"use strict";
+
+class AddNode {
+    constructor(playerTracker, item) {
+        this.playerTracker = playerTracker;
+        this.item = item;
+    }
+    build() {
+        let buffer = new Buffer(5);
+        buffer.writeUInt8(0x20, 0, 1);
+        buffer.writeUInt32LE((this.item.nodeID ^ this.playerTracker.scrambleID) >>> 0, 1, 1);
+        return buffer;
+    }
 }
 
 module.exports = AddNode;
-
-AddNode.prototype.build = function() {
-    var buffer = new Buffer(5);
-    buffer.writeUInt8(0x20, 0, 1);
-    buffer.writeUInt32LE((this.item.nodeID ^ this.playerTracker.scrambleID) >>> 0, 1, 1);
-    return buffer;
-};
