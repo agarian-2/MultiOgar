e0ceb627ad678c60cc007aad3b1ceb8316e4d1e9
Tom <36286279+agarian-2@users.noreply.github.com>
2022-06-13 15:58:47 -0400

    Update ClearOwned.js to ES6

--- src/packet/ClearOwned.js
+++ src/packet/ClearOwned.js
@@ -1,9 +1,11 @@
-function ClearOwned() {}
+"use strict";
+
+class ClearOwned {
+    build() {
+        let buffer = new Buffer(1);
+        buffer.writeUInt8(0x14, 0, 1);
+        return buffer;
+    }
+}
 
 module.exports = ClearOwned;
-
-ClearOwned.prototype.build = function() {
-    var buffer = new Buffer(1);
-    buffer.writeUInt8(0x14, 0, 1);
-    return buffer;
-};
